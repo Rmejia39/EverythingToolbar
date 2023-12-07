021bdb094a1a75d92abd6452f510c588c191233f
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-07 06:35:18 -0600

    update file

---
+++ EverythingToolbar/Converters/SearchResultsCountConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using EverythingToolbar.Properties;
+
+namespace EverythingToolbar.Converters
+{
+    public class SearchResultsCountConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "";
+
+            string formattedValue = ((int)value).ToString("N0", culture);
+
+            string suffix = (int)value == 1 ? Resources.SearchResult : Resources.SearchResults;
+            return $"{formattedValue} {suffix}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("This converter cannot be used in two-way binding.");
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
