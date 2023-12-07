1ff10027a0ac533bd972041a8baadc40c3576cfe
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-07 06:35:22 -0600

    update file

---
+++ EverythingToolbar/Helpers/ShellUtils.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+using NLog;
+
+namespace EverythingToolbar.Helpers
+{
+    class ShellUtils
+    {
+        private static readonly ILogger Logger = ToolbarLogger.GetLogger<ShellUtils>();
+        
+        private ShellUtils() { }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        static extern bool ShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct SHELLEXECUTEINFO
+        {
+            public int cbSize;
+            public uint fMask;
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpVerb;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpFile;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpParameters;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpDirectory;
+            public int nShow;
+            public IntPtr hInstApp;
+            public IntPtr lpIDList;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpClass;
+            public IntPtr hkeyClass;
+            public uint dwHotKey;
+            public IntPtr hIcon;
+            public IntPtr hProcess;
+        }
+
+        public static void ShowFileProperties(string path)
+        {
+            SHELLEXECUTEINFO info = new SHELLEXECUTEINFO();
+            info.cbSize = Marshal.SizeOf(info);
+            info.lpVerb = "properties";
+            info.lpFile = path;
+            info.nShow = 5;
+            info.fMask = 12;
+            ShellExecuteEx(ref info);
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+        public static void CreateProcessFromCommandLine(string commandLine, string workingDirectory = null)
+        {
+            var si = new STARTUPINFO();
+
+            CreateProcess(
+                null,
+                commandLine,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                false,
+                0,
+                IntPtr.Zero,
+                workingDirectory,
+                ref si,
+                out PROCESS_INFORMATION _);
+        }
+
+        public static void OpenWithDialog(string path)
+        {
+            var args = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "shell32.dll");
+            args += ",OpenAs_RunDLL " + path;
+            Process.Start("rundll32.exe", args);
+        }
+
+        private static bool WindowsExplorerIsDefault()
+        {
+            var folderShell = (string)Registry.ClassesRoot.OpenSubKey(@"Folder\shell")?.GetValue(null);
+            if (folderShell != null && Registry.ClassesRoot.OpenSubKey(@"Folder\shell\" + folderShell + @"\command")?.GetValue(null) != null)
+                return false;
+            
+            var directoryShell = (string)Registry.ClassesRoot.OpenSubKey(@"Directory\shell")?.GetValue(null);
+            if (directoryShell != null && Registry.ClassesRoot.OpenSubKey(@"Directory\shell\" + directoryShell + @"\command")?.GetValue(null) != null)
+                return false;
+
+            return true;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetShortPathName(
+            [MarshalAs(UnmanagedType.LPWStr)]
+            string path,
+            [MarshalAs(UnmanagedType.LPWStr)]
+            StringBuilder shortPath,
+            int shortPathLength
+        );
+
+        private static string GetShortPath(string path)
+        {
+            var shortPathBuilder = new StringBuilder(255);
+            var result = GetShortPathName(@"\\?\" + path, shortPathBuilder, shortPathBuilder.Capacity);
+            
+            if (result == 0)
+            {
+                Logger.Info($"Failed to get short path for '{path}'.");
+                return path;
+            }
+            
+            return shortPathBuilder.ToString();
+        }
+        
+        public static void OpenParentFolderAndSelect(string path)
+        {
+            if (WindowsExplorerIsDefault())
+            {
+                var shortPath = GetShortPath(path);
+                CreateProcessFromCommandLine($"explorer.exe /select,\"{shortPath}\"");
+                return;
+            }
+            
+            var parent = Path.GetDirectoryName(path) ?? path;
+            Process.Start(parent);
+        }
+    }
+}
